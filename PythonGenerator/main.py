572edca920383f6bc91f08efb9f05aa2bbe062ff
yuulkht <juliakukhtina12@gmail.com>
2024-09-13 01:20:26 +0300

    Initial commit

---
+++ PythonGenerator/main.py
@@ -0,0 +1,15 @@
+import random
+import pandas as pd
+import openpyxl
+
+variants_1 = []
+variants_2 = []
+for i in range(1000):
+    variants_1.append(str(random.randint(1, 9)) + "1" + str(random.randint(1000, 9999)))
+    variants_2.append(str(random.randint(1, 9)) + "2" + str(random.randint(1000, 9999)))
+variants_1 = list(map(lambda x: int(x), variants_1))
+variants_2 = list(map(lambda x: int(x), variants_2))
+
+df = pd.DataFrame({"Первые варианты:": variants_1, "Вторые варианты:": variants_2})
+
+df.to_excel('Номера вариантов вступы ЭМШ.xlsx')
